bab0ecb9075d1aab763509efee2eae48cba73514
Joshua Kennedy <josh@outthinkgroup.com>
2021-04-19 15:54:52 -0400

    finished update functionality

--- test-wp-automatic-plugin-update.php
+++ test-wp-automatic-plugin-update.php
@@ -17,92 +17,78 @@ define('WP_UPDATE_MANAGER_URL', plugin_dir_url(__FILE__));
 
 define('MY_PLUGINS_SLUG', "test-wp-automatic-plugin-update");
 
-add_filter('plugins_api', 'TEST_plugin_info', 20,3);
-function TEST_plugin_info($res, $action, $args){
-  if('plugin_information' !== $action ){
-    return false;
+/* 
+
+$transient: object with keys of all plugins information
+if our plugin needs to be updated we need to tell wordpress by adding it to the transient response list with our plugins information
+*/
+add_filter ( 'pre_set_site_transient_update_plugins', 'TEST_on_update_plugin');
+
+function TEST_on_update_plugin($transient){
+
+  if (empty ( $transient->checked )){
+        return $transient;
+  }            
+
+  $plugin='explore-wp-plugin-update/'.MY_PLUGINS_SLUG.'.php';
+  $new_version='7.0'; // this should be dynamic: wp_remote_get()
+  $plugin_info = get_site_transient('update_plugins');
+  $current_version = $plugin_info->checked[$plugin];
+
+  // make sure we do nothing when the current version seems to be greater than the "new" one
+  if (version_compare ( $current_version, $new_version, '>' )) {
+        return $transient;
   }
+  // create our response
+  $obj = new \stdClass ();
+  $obj->plugin = $plugin;
+  $obj->slug = MY_PLUGINS_SLUG;
+  $obj->new_version = $new_version;
+  $obj->package = 'https://raw.githubusercontent.com/joshatoutthink/explore-wp-plugin-update/main/explore-wp-plugin-update.zip';
 
-  $plugin_slug = MY_PLUGINS_SLUG;
-
-  if($plugin_slug !== $args->slug){
-    return false;
-  }
-
-  $remote = get_transient('update_tester_update'. $plugin_slug);
-  if($remote == false){
-
-    $remote = wp_remote_get('https://github.com/joshatoutthink/explore-wp-plugin-update/raw/main/info.json', [
-      'timeout'=>10,
-      'headers'=> [
-        'Accept' => 'application/json',
-      ]
-    ]);
-
-    if(! is_wp_error($remote) && isset($remote['response']['code']) && $remote['response']['code'] == 200 && !empty($remote['body'])) {
-      set_transient('update_tester_update'.$plugin_slug, $remote, 43200);
-    }
-  }
-
-  if(! is_wp_error($remote) && isset($remote['response']['code']) && $remote['response']['code'] == 200 && !empty($remote['body'])) {
-    $json = json_decode($remote['body']);
-    $res = new stdClass();
-
-    $res->name = $json->name;
-    $res->slug = $plugin_slug;
-    $res->version = $json->version;
-    $res->tested = $json->tested;
-    $res->requires = $json->requires;
-    $res->download_link = $json->download_url;
-    $res->trunk = $json->download_url;
-    $res->requires_php = '5.3';
-    $res->last_updated = $json->last_updated;
-
-    return $res;  
-  }
-
-  return false;
-  
+  // here we inject our response to the given $transient
+  $transient->response[$obj->plugin] = $obj;
+              
+  return $transient;
 }
 
-add_filter('site_transient_update_plugins', 'TEST_push_update');
-function TEST_push_update($transient){
-  
-  if(empty($transient->checked)){
-    return $transient;
+
+/* 
+This adds the details link and the update link if our plugin should update in the modal
+*/
+add_filter ( 'plugins_api', 'TEST_on_plugins_api', 10, 3 );
+
+function TEST_on_plugins_api($result, $action, $args) {
+
+  $plugin_slug=MY_PLUGINS_SLUG;
+  $plugin='explore-wp-plugin-update/'.MY_PLUGINS_SLUG.'.php';
+  if ($args->slug != $plugin_slug){
+    return $result;
   }
 
-  $remote = get_transient('update_tester_upgrade'. MY_PLUGINS_SLUG);
-  if($remote==false){
-    $remote = wp_remote_get('https://github.com/joshatoutthink/explore-wp-plugin-update/raw/main/info.json',[
-			'timeout' => 10,
-			'headers' => [
-				'Accept' => 'application/json'
-			] 
-    ]);
- 
-		if ( !is_wp_error( $remote ) && isset( $remote['response']['code'] ) && $remote['response']['code'] == 200 && !empty( $remote['body'] ) ) {
-			set_transient( 'update_tester_upgrade'. MY_PLUGINS_SLUG, $remote, 43200 ); // 12 hours cache
-		}
+  $plugin_info = get_site_transient ( 'update_plugins' );
+  $args->version = $plugin_info->checked[$plugin]; //
+  
+  // Create our object which should includes everything
+  // see wp-admin/includes/plugin-install.php
+  $obj = new \stdClass ();
 
-    if($remote){
-      $json = json_decode($remote['body']);
-
-      if($json && version_compare('1.0', $json->version, '<') && version_compare($remote->requires, get_bloginfo('version'), '<')){
-
-        $res = new stdClass();
-        
-        $res->slug = $json->slug;
-        $res->plugin = 'explore-wp-plugin-update/'.MY_PLUGINS_SLUG;
-        $res->new_version = $json->version;
-        $res->tested = $json->tested;
-        $res->package = $json->download_url;
-        
-        $transient->response[$res->plugin] = $res;
-      }
-    }
- 
-	}
-
-  return $transient;
+  $obj->version = $args->version;
+  $obj->new_version = '7.0';
+  $obj->author = 'Josh Kennedy';
+  $obj->slug = $plugin_slug;
+  $obj->name = 'TEST Wp Automatic Plugin Update';
+  $obj->plugin_name = $plugin_slug;
+  $obj->description = 'This is the plugin description';
+  $obj->requires = '3.0';
+  $obj->tested = '4.3.1';
+  $obj->last_updated = date ( 'Y-m-d' );
+  $obj->download_link = 'https://raw.githubusercontent.com/joshatoutthink/explore-wp-plugin-update/main/explore-wp-plugin-update.zip';
+  $obj->sections = array (
+                  'description' => 'A',
+                  'changelog' => '<h4>7.0</h4><ul><li>Fix - super cool fix</li><li>Tweak - super hot tweak</li></ul>',
+                  'another_section' => 'C' 
+      );
+  
+  return $obj;
 }
